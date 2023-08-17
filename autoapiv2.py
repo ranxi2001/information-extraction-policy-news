4df2339e42e4f7c6f8def8c49f1edc56654cbac2
arespollo <arespollo@outlook.com>
2023-08-17 19:53:41 +0800

    修改新版前端展示 后端是autoapiv2 前端是presentation

--- autoapiv2.py
+++ autoapiv2.py
@@ -5,40 +5,51 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 # 在这里定义你想要识别的实体类型
-# UIE具有zero-shot能力，所以类型可以随便定义，但是识别的好坏不一定
-schema = ['政策', '政府机构', '省份','城市地点','人名','人的代称','事件工程项目','会议','法规条例','组织机构企业','新闻杂志融媒体','国家']
-# 第一运行时，联网状态下会自动下载模型
-# device_id为gpu id，如果写-1则使用cpu,如果写0则使用gpu
-# ie = Taskflow('information_extraction', schema=schema, device_id=0,task_path='./doccano/uie/checkpoint/model_best/')
-ie = Taskflow('information_extraction', schema=schema, device_id=0,task_path='./uie-model/checkpoint/model_best3800')
+schema = [{"人名": "身份"},{'人的代称':"隶属"},{'政策':"发布单位"},{'政策':"针对群体"},{'法规条例':"发布单位"},{'法规条例':"针对群体"},{'事件工程项目':"发布单位"},{'事件工程项目':"针对群体"},{'会议':"发言人"},{'会议':"发布内容"},{'会议':"开会地点"},
+          '政府机构', '省份','城市地点','组织机构企业','新闻杂志融媒体','国家']
 
-def convert(result):
-    result = result[0]
-    formatted_result = []
-    for label, ents in result.items():
-        for ent in ents:
-            formatted_result.append(
-                {
-                    "label": label,
-                    "start_offset": ent['start'],
-                    "end_offset": ent['end']
-                })
+# 设定抽取目标和定制化模型权重路径
+my_ie = Taskflow("information_extraction", schema=schema, task_path='uie-model/checkpoint/model_best4000')
 
-    return formatted_result
-
+def entity_exists(entities, start, end):
+    for entity in entities:
+        if entity['start'] == start and entity['end'] == end:
+            return True
+    return False
 
 @app.route('/', methods=['POST'])
-def get_result():
+def process_text():
     text = request.json['text']
-    print(text)
-    result = ie(text)
-    formatted_result = convert(result)
-
-    return jsonify(formatted_result)
+    results = my_ie(text)
+    nodes = []
+    links = []
+    for result in results:  # 遍历列表中的每个字典
+        for key, values in result.items():  # 在每个字典上调用items()
+            for value in values:
+                nodes.append({
+                    'id': value['start'],
+                    'label': key,
+                    'text':value['text'],
+                    'start': value['start'],
+                    'end': value['end'],
+                    'probability': value['probability']
+                })
+                if 'relations' in value:
+                    for relation_type, related_entities in value['relations'].items():
+                        for related_entity in related_entities:
+                            links.append({
+                                'source': value['start'],
+                                'target': related_entity['start'],
+                                'sourceText': value['text'],
+                                'sourceLabel': key,
+                                'targetText': related_entity['text'],
+                                'relation': relation_type
+                            })
+    return jsonify({
+        'nodes': nodes,
+        'links': links
+    })
 
 
 if __name__ == '__main__':
-	# 这里写端口的时候一定要注意不要与已有的端口冲突
-	# 这里的host并不是说访问的时候一定要写0.0.0.0，但是这里代码要写0.0.0.0，代表可以被本网络中所有的看到
-	# 如果是其他机器访问你创建的服务，访问的时候要写你的ip
-    app.run(host='0.0.0.0', port=88)
+    app.run(host='0.0.0.0', port=888)
